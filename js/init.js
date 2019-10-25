7e98be412bb916dbb90993d7170f39ac300d8607
Redentity <38871974+lzted@users.noreply.github.com>
2019-10-25 19:24:45 +0800

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,14 @@
+// Edit the date here
+
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "22 November 2019 00:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
